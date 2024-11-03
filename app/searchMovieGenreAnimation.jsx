255dbb4b0857b1203a3856a4dcf852b3a7fb4cee
mjtc09 <mjtorres189@gmail.com>
2024-11-03 18:40:05 -0400

    Nuevas interfaces de Search Movie Genre, ya el icono back funciona, faltaria agregar cada una a la interfaz de SearchMovieGenre

---
+++ app/searchMovieGenreAnimation.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { View, StyleSheet, FlatList, useColorScheme } from 'react-native';
+import { Colors } from '../constants/Colors'; // Asegúrate de tener esta constante
+import { Themes } from '../constants/Themes';
+import { useRouter } from 'expo-router';
+import Header from '../components/Header'; // Importar el componente Header
+
+const movies = Array.from({ length: 18 }, (_, index) => ({ id: index.toString() }));
+
+const SearchMovieGenreAnimation = () => {
+  const router = useRouter();
+  const colorScheme = useColorScheme();
+  const themeColors = Colors[colorScheme] || Colors.light;
+
+  return (
+    <View style={[styles.container, { backgroundColor: themeColors.background }]}>
+      {/* Header */}
+      <Header 
+        title="Animation" 
+        leftIconName="arrow-back" 
+        leftIconRoute="/searchMovieGenre" // Ruta a la que navegar
+      />
+
+      {/* Movie Grid */}
+      <FlatList
+        data={movies}
+        keyExtractor={(item) => item.id}
+        numColumns={3}
+        renderItem={() => (
+          <View style={styles.movieContainer} />
+        )}
+        contentContainerStyle={styles.grid}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  grid: {
+    padding: 10,
+  },
+  movieContainer: {
+    flex: 1,
+    margin: 5,
+    height: 165, // Altura de cada recuadro en la cuadrícula
+    backgroundColor: Themes.colors.grayDark, // Color de fondo de los recuadros
+    borderRadius: 8,
+  },
+});
+
+export default SearchMovieGenreAnimation;
